eb0f4329fbd015b64ee887fd3f2167c25b12dd7d
KVECKO <samo.knieza@gmail.com>
2024-11-13 10:56:37 +0100

    Employee DTO full

---
+++ DTOs/Employee/EmployeeDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace DTOs;
+
+public class EmployeeDTO
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int WorkingNumber { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+}
