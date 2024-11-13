7f18338287f964e8f02b73ebe36acdd6ec92a9b9
Marius Gafton <mariusgafton2005@gmail.com>
2024-11-13 10:57:21 +0100

    Wrote the draft of entities dotNet in branch Entities

---
+++ Entities/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace Entities;
+
+public class Employee
+{
+    public Employee(string firstName, string lastName, int workingNumber, string email, int phoneNumber)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        WorkingNumber = workingNumber;
+        Email = email;
+        PhoneNumber = phoneNumber;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int WorkingNumber { get; set; }
+    public string Email { get; set; }
+    public int PhoneNumber { get; set; }
+}
