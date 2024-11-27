42b23871c99e1ec07adea6681cfa3b025514e1d5
KVECKO <samo.knieza@gmail.com>
2024-11-27 18:06:43 +0100

    proposals

---
+++ Entities/ShiftSwitchRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class ShiftSwitchRequest
+{
+    public Shift OriginShift { get; set; }
+    public Employee OriginEmployee { get; set; }
+    public long Id { get; set; }
+    public string Details { get; set; }
+    public List<ShiftSwitchRequestTimeframe> Timeframes { get; set; }
+    public List<ShiftSwitchReply> SwitchReplies { get; set; }
+}
