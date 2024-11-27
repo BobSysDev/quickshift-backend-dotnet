a92c8fa9100c08c0bacb756c974f8a7a2808ff8e
Marius Gafton <mariusgafton2005@gmail.com>
2024-11-27 15:12:08 +0100

    added shiftproposal and shiftproposaldto

---
+++ Entities/ShiftProposal.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Entities
+{
+    public class ShiftProposal
+    {
+        
+        public long Id { get; set; }
+        
+        public Shift OriginShift { get; set; }
+        public long OriginShiftId { get; set; }
+        
+        public Employee OriginEmployee { get; set; }
+        public long OriginEmployeeId { get; set; }
+        
+        public Shift TargetShift { get; set; }
+        public long TargetShiftId { get; set; }
+        
+        public Employee TargetEmployee { get; set; }
+        public long TargetEmployeeId { get; set; }
+    }
+}
