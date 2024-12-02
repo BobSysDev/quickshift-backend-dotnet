4915326fb35dd372c112fe5db6563bb39a400b06
SebinoBR <sbrbartko@gmail.com>
2024-12-02 09:38:45 +0100

    updated protos

---
+++ Entities/SwitchShiftReply.cs
@@ -0,0 +1,11 @@
+﻿namespace Entities;
+
+public class SwitchShiftReply
+{
+    public long Id { get; set; }
+    public Shift TargetShift { get; set; }
+    public Employee TargetEmployee { get; set; }
+    public bool TargetAccepted { get; set; }
+    public bool OriginAccepted { get; set; }
+    public string Details { get; set; }
+}
