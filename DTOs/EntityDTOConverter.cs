42b049c5dc45b5c6b6eff03ed19c9091376cd02b
KVECKO <samo.knieza@gmail.com>
2024-12-04 10:59:34 +0100

    Entity to Dto for employees skeleton

---
+++ DTOs/EntityDTOConverter.cs
@@ -0,0 +1,76 @@
+using Entities;
+
+namespace DTOs;
+
+public class EntityDTOConverter
+{
+    //all emp. dtos to emp. entity
+    public static Employee EmployeeDtoToEmployee(EmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static Employee AuthEmployeeDtoToEmployee(AuthEmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    public static Employee NewEmployeeDtoToEmployee(NewEmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    public static Employee PublicEmployeeDtoToEmployee(PublicEmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    public static Employee ShiftEmployeeDtoToEmployee(ShiftEmpoyeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    public static Employee SimpleEmployeeDtoToEmployee(SimpleEmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    public static Employee UpdateEmployeeDtoToEmployee(UpdateEmployeeDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+    
+    
+    
+    //all emp. entity to emp. dtos
+    
+    public static AuthEmployeeDTO EmployeeToAuthEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static EmployeeDTO EmployeeToEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static NewEmployeeDTO EmployeeToNewEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static PublicEmployeeDTO EmployeeToPublicEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static ShiftEmpoyeeDTO EmployeeToShiftEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static SimpleEmployeeDTO EmployeeToSimpleEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+    
+    public static UpdateEmployeeDTO EmployeeToUpdateEmployeeDto(Employee e)
+    {
+        throw new NotImplementedException();
+    }
+}
