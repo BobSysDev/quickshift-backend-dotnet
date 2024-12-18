ecaa86e956ea0e3c30464f4ae4dfc58c3a2df68c
Electimore <electimore@gmail.com>
2024-12-18 14:48:54 +0100

    cleaning up after Kvecko's commit

--- RestAPI/Controllers/EmployeeController.cs
+++ RestAPI/Controllers/EmployeeController.cs
@@ -56,9 +56,7 @@ public class EmployeeController : ControllerBase
     {
         try
         {
-            
-            
-            Employee existingEmployee = await employeeRepo.GetSingleAsync(long.CreateChecked(id));
+            Employee? existingEmployee = await employeeRepo.GetSingleAsync(long.CreateChecked(id));
             if (existingEmployee == null)
             {
                 return NotFound($"Employee with the ID {id} not found");
@@ -67,9 +65,10 @@ public class EmployeeController : ControllerBase
             existingEmployee.LastName = request.LastName;
             existingEmployee.WorkingNumber = request.WorkingNumber;
             existingEmployee.Email = request.Email;
-            //existingEmployee.Shifts = EmployeeGrpcRepository.EntityShiftDtosToEntityShiftsList(request.Shifts);
-            //existingEmployee.Id = request.Id;
-            existingEmployee.Password = AuthController.Hash(request.Password);
+            if (request.Password != "")
+            {
+                existingEmployee.Password = AuthController.Hash(request.Password);
+            }
             existingEmployee.IsManager = request.IsManager;
 
             Employee updated = await employeeRepo.UpdateAsync(existingEmployee);
